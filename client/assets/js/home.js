646a155c4d53923cab7fedf123433348d4f1debd
SevilHs <7l2zfk3@code.edu.az>
2023-06-08 07:34:12 +0400

    fix: style of extra-services section & home.js fixed

--- client/assets/js/home.js
+++ client/assets/js/home.js
@@ -13,13 +13,16 @@ $('.owl-carousel').owlCarousel({
   loop:true,
   margin:10,
   nav:true,
-  items: 2,
-  // responsive:{
-  //     0:{
-  //         items:1
-  //     },
-  //     1000:{
-  //         items:4
-  //     }
-  // }
+  items:2,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      },
+      1000:{
+          items:2
+      }
+  }
 })
