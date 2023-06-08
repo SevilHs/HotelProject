e10faa01ec64c280fbc30091207fde4d27999b18
SevilHs <7l2zfk3@code.edu.az>
2023-06-08 05:47:13 +0400

    feat: services section created $ carousel added

--- client/assets/js/home.js
+++ client/assets/js/home.js
@@ -8,3 +8,18 @@ menuBtn.addEventListener("click", () => {
     menuItems.style.display = "block";
   }
 });
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  items: 2,
+  // responsive:{
+  //     0:{
+  //         items:1
+  //     },
+  //     1000:{
+  //         items:4
+  //     }
+  // }
+})
