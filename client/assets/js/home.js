323f0fe293ae36c6085f9ac5ca38aae3a4e47cae
SevilHs <7l2zfk3@code.edu.az>
2023-06-09 22:16:42 +0400

    fix: updated some files & testimonials.scss created

--- client/assets/js/home.js
+++ client/assets/js/home.js
@@ -9,11 +9,12 @@ menuBtn.addEventListener("click", () => {
   }
 });
 
-$('.owl-carousel').owlCarousel({
+$('.owl-carousel1').owlCarousel({
   loop:true,
   margin:10,
   nav:true,
   items:2,
+  autoplay:true,
   responsive:{
       0:{
           items:1
@@ -26,3 +27,24 @@ $('.owl-carousel').owlCarousel({
       }
   }
 })
+
+var owl = $('.owl-carousel2');
+owl.owlCarousel({
+    items:1, 
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:1
+      },
+      1000:{
+          items:1
+      }
+  }
+});
