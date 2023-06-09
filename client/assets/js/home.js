6d619b6a6e227b5d93ef3cbf7ad654364d4cdd8a
SevilHs <7l2zfk3@code.edu.az>
2023-06-10 03:08:38 +0400

    feat: created news section & home.js updated

--- client/assets/js/home.js
+++ client/assets/js/home.js
@@ -48,3 +48,22 @@ owl.owlCarousel({
       }
   }
 });
+
+$('.owl-carousel3').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  items:3,
+  autoplay:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      },
+      1000:{
+          items:3
+      }
+  }
+})
