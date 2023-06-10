fd5e75de81883a1556e02a0e3b7759ee67f4ff52
SevilHs <7l2zfk3@code.edu.az>
2023-06-10 15:38:36 +0400

    fix: home.js update for client section

--- client/assets/js/home.js
+++ client/assets/js/home.js
@@ -72,6 +72,25 @@ $(".owl-carousel3").owlCarousel({
   },
 });
 
+$(".owl-carousel4").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  items: 3,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
+
 function showNot(){
   if (!option1.value || !option2.value) {
     not.removeAttribute("hidden");
