e7dd0f32ff9aa4f8afb38c32bef3db0e7c774d8b
SevilHs <7l2zfk3@code.edu.az>
2023-06-10 10:30:48 +0400

    feat: booking section added & home.js updated

--- client/assets/js/home.js
+++ client/assets/js/home.js
@@ -1,5 +1,9 @@
 let menuBtn = document.querySelector(".menu-btn");
 let menuItems = document.querySelector(".menu-items");
+let form = document.querySelector("#booking-form");
+let option1 = document.querySelector("#adults"); 
+let option2 = document.querySelector("#children"); 
+let not = document.querySelector("#notification");
 
 menuBtn.addEventListener("click", () => {
   if (menuItems.style.display == "block") {
@@ -9,61 +13,76 @@ menuBtn.addEventListener("click", () => {
   }
 });
 
-$('.owl-carousel1').owlCarousel({
-  loop:true,
-  margin:10,
-  nav:true,
-  items:2,
-  autoplay:true,
-  responsive:{
-      0:{
-          items:1
-      },
-      600:{
-          items:2
-      },
-      1000:{
-          items:2
-      }
-  }
-})
-
-var owl = $('.owl-carousel2');
-owl.owlCarousel({
-    items:1, 
-    loop:true,
-    margin:10,
-    autoplay:true,
-    autoplayTimeout:2000,
-    autoplayHoverPause:true,
-    responsive:{
-      0:{
-          items:1
-      },
-      600:{
-          items:1
-      },
-      1000:{
-          items:1
-      }
-  }
+$(".owl-carousel1").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  items: 2,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 2,
+    },
+  },
 });
 
-$('.owl-carousel3').owlCarousel({
-  loop:true,
-  margin:10,
-  nav:true,
-  items:3,
-  autoplay:true,
-  responsive:{
-      0:{
-          items:1
-      },
-      600:{
-          items:2
-      },
-      1000:{
-          items:3
-      }
+var owl = $(".owl-carousel2");
+owl.owlCarousel({
+  items: 1,
+  loop: true,
+  margin: 10,
+  autoplay: true,
+  autoplayTimeout: 2000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
+
+$(".owl-carousel3").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  items: 3,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
+
+function showNot(){
+  if (!option1.value || !option2.value) {
+    not.removeAttribute("hidden");
   }
-})
+  setTimeout(() => {
+    notification.setAttribute("hidden", "");
+  }, 2000);
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  showNot()
+  console.log(option1.value);
+});
