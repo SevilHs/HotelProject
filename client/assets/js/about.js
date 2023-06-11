8649d623c6c4186076a2f1719f9f37a465ededb3
SevilHs <7l2zfk3@code.edu.az>
2023-06-11 04:36:26 +0400

    feat: created team section

---
+++ client/assets/js/about.js
@@ -0,0 +1,18 @@
+$(".owl-carousel1").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    items: 2,
+    autoplay: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 2,
+      },
+    },
+  });
