a3df4aa477a6f68bec0ed579431b99f0ddb6ad75
SevilHs <7l2zfk3@code.edu.az>
2023-06-11 04:41:53 +0400

    fix: about.js fixed & some changes

--- client/assets/js/about.js
+++ client/assets/js/about.js
@@ -2,7 +2,7 @@ $(".owl-carousel1").owlCarousel({
     loop: true,
     margin: 10,
     nav: true,
-    items: 2,
+    items: 3,
     autoplay: true,
     responsive: {
       0: {
@@ -12,7 +12,7 @@ $(".owl-carousel1").owlCarousel({
         items: 2,
       },
       1000: {
-        items: 2,
+        items: 3,
       },
     },
   });
