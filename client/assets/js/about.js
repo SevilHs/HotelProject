fbcd149c2e1040ee12743e233934f40eab9731c2
SevilHs <7l2zfk3@code.edu.az>
2023-06-11 19:25:54 +0400

    fix: about.js update for testimonials section

--- client/assets/js/about.js
+++ client/assets/js/about.js
@@ -19,22 +19,37 @@ $(".owl-carousel1-about").owlCarousel({
 
 let numbers = document.querySelectorAll(".num");
 let number1=document.querySelector('.num1')
-let scrollTopCounter=document.querySelector('#counter')
+let counterSec=document.querySelector('#counter')
 
-numbers.forEach((number) => {
-  const updateNumber=()=>{
-    let endValue=parseInt(number.getAttribute('data-value'))
-    // console.log(endValue);
-    let numberText= parseInt(number.innerHTML)
-    // console.log(numberText);
-    let increment= endValue/200
-    // console.log(increment);
-    if(numberText<endValue){
-      number.innerHTML=Math.ceil(numberText+increment)
-      setTimeout(updateNumber,1)
-    }else if(number1.innerHTML==endValue){
-     number1.innerHTML=`${endValue}+`
-    }
-  } 
-  updateNumber()
+
+// console.log(counterSec.getBoundingClientRect().top);
+// console.log(document.documentElement.scrollTop);
+let scrollValue=counterSec.offsetTop
+window.onscroll=(()=>{
+  let documentScrollValue=document.documentElement.scrollTop
+  // console.log(scrollValue);
+  // console.log(documentScrollValue);
+  if (documentScrollValue>2000) {
+    numbers.forEach((number) => {
+      const updateNumber=()=>{
+        let endValue=parseInt(number.getAttribute('data-value'))
+        // console.log(endValue);
+        let numberText= parseInt(number.innerHTML)
+        // console.log(numberText);
+        let increment= endValue/200
+        // console.log(increment);
+        if(numberText<endValue){
+          number.innerHTML=Math.ceil(numberText+increment)
+          setTimeout(updateNumber,1)
+        }else if(number1.innerHTML==endValue){
+         number1.innerHTML=`${endValue}+`
+        }
+      } 
+      updateNumber()
+    });
+  }
+})
+
+$('input').on('change', function() {
+  $('#testimonials-about').toggleClass('change-bgcolor');
 });
