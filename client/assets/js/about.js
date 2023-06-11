5c85729095838549e999232e55be8e74a52cca19
SevilHs <7l2zfk3@code.edu.az>
2023-06-11 15:56:15 +0400

    fix: about.js update for counter section

--- client/assets/js/about.js
+++ client/assets/js/about.js
@@ -1,18 +1,40 @@
 $(".owl-carousel1-about").owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: true,
-    items: 3,
-    autoplay: true,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      600: {
-        items: 2,
-      },
-      1000: {
-        items: 3,
-      },
+  loop: true,
+  margin: 10,
+  nav: true,
+  items: 3,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 1,
     },
-  });
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
+
+let numbers = document.querySelectorAll(".num");
+let number1=document.querySelector('.num1')
+let scrollTopCounter=document.querySelector('#counter')
+
+numbers.forEach((number) => {
+  const updateNumber=()=>{
+    let endValue=parseInt(number.getAttribute('data-value'))
+    // console.log(endValue);
+    let numberText= parseInt(number.innerHTML)
+    // console.log(numberText);
+    let increment= endValue/200
+    // console.log(increment);
+    if(numberText<endValue){
+      number.innerHTML=Math.ceil(numberText+increment)
+      setTimeout(updateNumber,1)
+    }else if(number1.innerHTML==endValue){
+     number1.innerHTML=`${endValue}+`
+    }
+  } 
+  updateNumber()
+});
