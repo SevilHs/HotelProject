6a3ef381d303fe996961313033827aedd777feed
SevilHs <7l2zfk3@code.edu.az>
2023-06-13 04:22:25 +0400

    fix: fixed counter & some bugs

--- client/assets/js/about.js
+++ client/assets/js/about.js
@@ -17,38 +17,49 @@ $(".owl-carousel1-about").owlCarousel({
   },
 });
 
-let numbers = document.querySelectorAll(".num");
-let number1=document.querySelector('.num1')
-let counterSec=document.querySelector('#counter')
 
+// window.onscroll=(()=>{
+//   let a=0
+//   let oTop= counterSec.offsetTop-window.innerHeight
+//   if (a==0 && document.documentElement.scrollTop>oTop) {
+//     numbers.forEach((number) => {
+//       const updateNumber=()=>{
+//         let endValue=parseInt(number.getAttribute('data-value'))
+//         // console.log(endValue);
+//         let numberText= parseInt(number.innerHTML)
+//         // console.log(numberText);
+//         let increment= endValue/200
+//         // console.log(increment);
+//         if(numberText<endValue){
+//           number.innerHTML=Math.ceil(numberText+increment)
+//           setTimeout(updateNumber,1)
+//         }else if(number1.innerHTML==endValue){
+//          number1.innerHTML=`${endValue}+`
+//         }
+//       }
+//       updateNumber()
+//     });
+//     a=1
+//   }
+// })
 
-// console.log(counterSec.getBoundingClientRect().top);
-// console.log(document.documentElement.scrollTop);
-let scrollValue=counterSec.offsetTop
-window.onscroll=(()=>{
-  let documentScrollValue=document.documentElement.scrollTop
-  // console.log(scrollValue);
-  // console.log(documentScrollValue);
-  if (documentScrollValue>2000) {
-    numbers.forEach((number) => {
-      const updateNumber=()=>{
-        let endValue=parseInt(number.getAttribute('data-value'))
-        // console.log(endValue);
-        let numberText= parseInt(number.innerHTML)
-        // console.log(numberText);
-        let increment= endValue/200
-        // console.log(increment);
-        if(numberText<endValue){
-          number.innerHTML=Math.ceil(numberText+increment)
-          setTimeout(updateNumber,1)
-        }else if(number1.innerHTML==endValue){
-         number1.innerHTML=`${endValue}+`
-        }
-      } 
-      updateNumber()
+var a = 0;
+$(window).scroll(function() {
+  var oTop = $('#counter').offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $('.num').each(function() {
+        var $this = $(this);
+        jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+            duration: 2000,
+            easing: 'swing',
+            step: function () {
+                $this.text(Math.ceil(this.Counter));
+            }
+        });
     });
+    a = 1;
   }
-})
+});
 
 $('input').on('change', function() {
   $('#testimonials-about').toggleClass('change-bgcolor');
--- client/assets/js/restaurant.js
+++ client/assets/js/restaurant.js
@@ -1,6 +1,6 @@
 let arrowDown=document.querySelector('.arrow-down')
-let visitUs= document.querySelector('#visit-us')
-let {top: position}=visitUs.getBoundingClientRect()
+let aboutRestSec= document.querySelector('#about-rest')
+let {top: position}=aboutRestSec.getBoundingClientRect()
 console.log(position);
 arrowDown.addEventListener('click',()=>{
     document.documentElement.scrollTop=position
