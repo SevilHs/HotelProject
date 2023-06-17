8563b721755d508e8a2bac135fce1b35db07be2c
SevilHs <7l2zfk3@code.edu.az>
2023-06-17 10:04:31 +0400

    fix: some changes & favicon added

--- client/assets/js/signin.js
+++ client/assets/js/signin.js
@@ -4,24 +4,38 @@ let form = document.querySelector("form");
 let username = document.querySelector(".username");
 let password = document.querySelector(".password");
 let showPassword = document.querySelector(".fa-eye");
+let requiredText = document.querySelectorAll(".required-text");
+
+requiredText.forEach((item) => (item.style.visibility = "hidden"));
+
+username.addEventListener("input", () => {
+  requiredText[0].style.visibility = "hidden";
+});
+password.addEventListener("input", () => {
+  requiredText[1].style.visibility = "hidden";
+});
 
 let checkShow = false;
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  async function getUser() {
-    let res = await axios(`${BASE_URL}/users`);
-    let data = res.data;
-    data.filter((user) => {
-      (user.username.toLocaleLowerCase() ==
-        username.value.toLocaleLowerCase() ||
-        user.email.toLocaleLowerCase() == username.value.toLocaleLowerCase()) &&
-      user.password.toLocaleLowerCase() == password.value.toLocaleLowerCase()
-        ? console.log("true")
-        : console.log("false");
-    });
+  if (username.value && password.value) {
+      async function getUser() {
+        let res = await axios(`${BASE_URL}/users`);
+        let data = res.data;
+        data.filter((user) => {
+          (user.username.toLocaleLowerCase() ==
+            username.value.toLocaleLowerCase() ||
+            user.email.toLocaleLowerCase() == username.value.toLocaleLowerCase()) &&
+          user.password.toLocaleLowerCase() == password.value.toLocaleLowerCase()
+            ? console.log("true")
+            : console.log("false");
+        });
+      }
+      getUser();
+  } else {
+    requiredText.forEach((item) =>(item.style.visibility = "visible"));
   }
-  getUser();
   //   window.location = "shop.html";
 });
 
