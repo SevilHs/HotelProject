bbc995d07cf398d919a15d4e68b91fdca504d7f1
SevilHs <7l2zfk3@code.edu.az>
2023-06-20 22:07:46 +0400

    fix: users form fixed

--- client/assets/js/signup.js
+++ client/assets/js/signup.js
@@ -7,6 +7,9 @@ let number = document.querySelector(".number");
 let password = document.querySelector(".password");
 let showPassword = document.querySelector(".fa-eye");
 let requiredText = document.querySelectorAll(".required-text");
+let file=document.querySelector('.file')
+
+let base64
 
 requiredText.forEach((item) => (item.style.visibility = "hidden"));
 username.addEventListener('input',()=>{
@@ -31,22 +34,25 @@ function emptyInp() {
   password.value = "";
 }
 
+// let userData = JSON.parse(localStorage.getItem("sign")) || [];
 async function setUser() {
   let obj = {
     username: username.value,
     email: email.value,
     number: number.value,
     password: password.value,
+    file:base64 ? base64 : './assets/images/home-images/user.png',
     isAdmin:false
   };
+  // userData.push(obj)
   let res = await axios(`${BASE_URL}/users`);
   let data = res.data;
   let checkData = data.find((item) => item.username == username.value);
   console.log(checkData);
   if (!checkData) {
+    // localStorage.setItem("sign", JSON.stringify(userData))
     await axios.post(`${BASE_URL}/users`, obj);
     emptyInp();
-    localStorage.setItem("sign","true")
     window.location="./shop.html"
   } else {
     alert("this username already exists");
@@ -72,3 +78,30 @@ showPassword.addEventListener("click", () => {
     checkShow = false;
   }
 });
+
+const convertBase64=(file)=>{
+  return new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+
+    fileReader.onload = () => {
+      resolve(fileReader.result);
+    };
+
+    fileReader.onerror = (error) => {
+      reject(error);
+    };
+  });
+}
+
+const uploadImg= async(e)=>{
+  const file=e.target.files[0]
+  base64=await convertBase64(file)
+  // console.log(file);
+  // console.log(e);
+  // console.log(base64);
+}
+
+file.addEventListener('change',(e)=>{
+  uploadImg(e)
+})
